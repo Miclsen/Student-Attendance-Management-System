6a3b58bb88eea95f0cbdb0b78756d566527c037e
Mark Justin Tabarez <markjustintabarez@gmail.com>
2026-09-19 04:16:26 +0800

    student class

--- Student-Attendance-Management-System/Program.cs
+++ Student-Attendance-Management-System/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace AttendanceSystem
+{
+    // Represents a single student in the attendance system.
+    class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string Course { get; set; }
+    }
+}
